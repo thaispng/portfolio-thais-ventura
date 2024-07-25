13346d7cd0aafa51d5ff0a8bebbf0274b3466731
thais ventura <thaisvtr26@gmail.com>
2024-07-24 21:20:40 -0300

    :sparkles: feat: Coloquei os componentes criados na página de page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import Header from "@/components/header";
 import SideMenu from "@/components/SideMenu";
+import Inicial from "./Inicial";
+import Habilidades from "./Habilidades";
+import Experiencia from "./Experiencia";
+import Projetos from "./Projetos";
 export default function Home() {
   return (
     <>
   
       <Header />
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container-bg flex flex-row justify-between p-5 gap-2 w-h-[100vh]">
       <SideMenu />
+      <div className="flex flex-col gap-2">
+      <Inicial />
+      <Habilidades />
+      <Experiencia />
+      <Projetos />
+      </div>
     </main>
     </>
   );
