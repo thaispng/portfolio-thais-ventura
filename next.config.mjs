54c73daae201b6e4410f1bd8098e0c2d403eefe4
thais ventura <thaisvtr26@gmail.com>
2024-07-24 21:12:14 -0300

    :heavy_plus_sign:build: @svgr/webpack react-dom tech-stack-icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: [], 
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      });
+  
+      return config;
+    }
+  };
+  
+  export default nextConfig;
+  
