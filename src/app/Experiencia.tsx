5f0345f883b0c392c5c982f968b1288e686f82e6
thais ventura <thaisvtr26@gmail.com>
2024-07-24 21:19:31 -0300

    :sparkles: feat: Componente experiência

---
+++ src/app/Experiencia.tsx
@@ -0,0 +1,244 @@
+import React from "react";
+import SubTitle from "@/components/SubTitle";
+export default function Inicial() {
+  return (
+    <>
+        <div className="container-bg gap-10 rounded-md border-color border-2 flex flex-col w-full  items-start justify-start p-10 ">
+        <div className="flex flex-col">
+        <h1 className="text-primary text-[30px] font-bold">
+            Experiências
+          </h1>
+            <SubTitle subTitle="Minha Jornada Profissional." />
+        </div>
+        <div className="flex flex-col container-bg-tertiary w-full h-full py-4 px-4 rounded-md gap-4">
+  <div className="flex flex-row justify-between">
+    <p className="text-primary font-semibold break-all whitespace-normal">
+      Desenvolvedora Front-end
+    </p>
+    <SubTitle subTitle="Julho de 2024 - Atual" />
+  </div>
+  <div className="flex flex-col justify-between pt-4">
+    <p className="text-secondary font-semibold text-sm">
+      Empresa
+    </p>
+    <SubTitle subTitle="GR Digital" />
+  </div>
+  <div>
+    <p className="text-secondary font-semibold text-sm">
+      Descrição da vaga
+    </p>
+    <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+      Sou desenvolvedora júnior front-end, especializada em Next.js e Tailwind CSS. Atualmente, estou desenvolvendo um sistema interno, focando na criação de aplicações web modernas e responsivas. Adoto uma arquitetura de projeto limpa, garantindo a escalabilidade e modularidade do código. Utilizo práticas de CI/CD com ferramentas como GitHub Actions para automatizar builds, testes e deploys, assegurando entregas ágeis e de alta qualidade.
+    </p>
+  </div>
+  <div>
+    <p className="text-secondary font-semibold text-sm">
+      Tecnologias
+    </p>
+    <ul className="list-disc list-inside text-tertiary font-normal indent-5 break-all whitespace-normal">
+      <li>Next.js</li>
+      <li>TypeScript</li>
+      <li>Tailwind CSS</li>
+      <li>GitHub Actions</li>
+      <li>Jira</li>
+      <li>Discord </li>
+    </ul>
+  </div>
+</div>
+
+<div className="flex flex-col container-bg-tertiary w-full h-full py-4 px-4 rounded-md gap-4">
+  <div className="flex flex-row justify-between">
+    <p className="text-primary font-semibold break-all whitespace-normal">
+      Desenvolvedor Front-end
+    </p>
+    <SubTitle subTitle="Setembro de 2023 - junho de 2024" />
+  </div>
+  <div className="flex flex-col justify-between pt-4">
+    <p className="text-secondary font-semibold text-sm">
+      Empresa
+    </p>
+    <SubTitle subTitle="MF Digital Law" />
+  </div>
+  <div>
+    <p className="text-secondary font-semibold text-sm">
+      Descrição da vaga
+    </p>
+    <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+      Em colaboração com minha equipe na MF Digital Law, desenvolvi e implementei um novo sistema interno para a empresa, focado em aprimorar a eficiência e a experiência do usuário. Como desenvolvedor front-end, trabalhei intensamente na criação de interfaces interativas e responsivas, utilizando tecnologias modernas para garantir um desempenho robusto.
+    </p>
+    <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+      Além do desenvolvimento, também liderei a parte de design UX/UI, realizando pesquisas de usabilidade para entender as necessidades dos usuários e definir requisitos claros. Utilizei ferramentas como Figma e Sketch para projetar e prototipar a interface, assegurando uma experiência de usuário intuitiva e eficiente. Elaborei documentação detalhada para guiar o processo de desenvolvimento, garantindo a coerência e a qualidade do projeto.
+    </p>
+  </div>
+  <div>
+    <p className="text-secondary font-semibold text-sm">
+      Tecnologias
+    </p>
+    <ul className="list-disc list-inside text-tertiary font-normal indent-5 break-all whitespace-normal">
+      <li>Next.js</li>
+      <li>TypeScript</li>
+      <li>Tailwind CSS</li>
+      <li>Nextron.js</li>
+      <li>Express.js</li>
+      <li>TypeScript</li>
+      <li>GitHub Actions</li>
+      <li>Figma</li>
+      <li>Sketch</li>
+      <li>Jira</li>
+      <li>Slack</li>
+    </ul>
+  </div>
+</div>
+
+<div className="flex flex-col container-bg-tertiary w-full h-full py-4 px-4 rounded-md gap-4">
+  <div className="flex flex-row justify-between">
+    <p className="text-primary font-semibold break-all whitespace-normal">
+      Design UX/UI
+    </p>
+    <SubTitle subTitle="Outubro de 2023 - Desembro de 2023" />
+  </div>
+  <div className="flex flex-col justify-between pt-4">
+    <p className="text-secondary font-semibold text-sm">
+      Empresa
+    </p>
+    <SubTitle subTitle="StandShow" />
+  </div>
+  <div>
+    <p className="text-secondary font-semibold text-sm">
+      Descrição da vaga
+    </p>
+    <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+      Responsável pelo design UX/UI do sistema interno da empresa StandShow.
+    </p>
+    <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+      Concepção e criação de interfaces de usuário intuitivas e atraentes, utilizando ferramentas e tecnologias como Adobe XD, Figma, Sketch e InVision para prototipagem e wireframing.
+    </p>
+    <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+      Implementação de design responsivo e princípios de design centrado no usuário para melhorar a experiência e a usabilidade do sistema.
+    </p>
+    <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+      Colaboração com equipes de desenvolvimento para garantir a consistência visual e funcional em todas as plataformas e dispositivos, utilizando metodologias ágeis e ferramentas de colaboração como Jira e Slack.
+    </p>
+</div>
+
+<div>
+    <p className="text-secondary font-semibold text-sm">
+      Tecnologias
+    </p>
+    <ul className="list-disc list-inside text-tertiary font-normal indent-5 break-all whitespace-normal">
+      <li>Adobe XD</li>
+      <li>Figma</li>
+      <li>Sketch</li>
+      <li>InVision</li>
+      <li>Jira</li>
+      <li>Slack</li>
+    </ul>
+</div>
+</div>
+
+<div className="flex flex-col container-bg-tertiary w-full h-full py-4 px-4 rounded-md gap-4">
+  <div className="flex flex-row justify-between">
+    <p className="text-primary font-semibold break-all whitespace-normal">
+     Desenvolvedora Front-end e Design UX/UI
+    </p>
+    <SubTitle subTitle="Setembro de 2023 - junho de 2024" />
+  </div>
+  <div className="flex flex-col justify-between pt-4">
+    <p className="text-secondary font-semibold text-sm">
+      Empresa
+    </p>
+    <SubTitle subTitle="Agilean" />
+  </div>
+  <div>
+  <p className="text-secondary font-semibold text-sm">
+    Descrição da vaga
+  </p>
+  <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+    Responsável pelo design UX/UI do sistema voltado para o público da empresa Agilean.
+  </p>
+  <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+    Concepção e criação de interfaces de usuário intuitivas e atraentes, utilizando ferramentas e tecnologias como Figma e Miro para prototipagem e wireframing.
+  </p>
+  <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+    Condução de pesquisas de usabilidade para compreender as expectativas dos usuários e definir os requisitos do sistema de forma precisa.
+  </p>
+  <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+    Implementação de design responsivo e princípios de design centrado no usuário para melhorar a experiência e a usabilidade do sistema.
+  </p>
+  <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+    Desenvolvimento do sistema utilizando React para a criação de componentes reutilizáveis e React Native para a extensão em plataformas móveis, assegurando uma experiência consistente entre dispositivos.
+  </p>
+  <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+    Utilização do Git para controle de versão, facilitando a colaboração eficiente e a gestão organizada do código.
+  </p>
+  <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+    Colaboração com equipes de desenvolvimento para garantir a consistência visual e funcional em todas as plataformas e dispositivos, utilizando metodologias ágeis e ferramentas de colaboração como Jira e Slack.
+  </p>
+  <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+    O resultado final foi um sistema moderno, responsivo e centrado no usuário, que superou as expectativas do público-alvo, proporcionando uma experiência de usuário aprimorada e contribuindo para o aumento da satisfação e engajamento dos clientes.
+  </p>
+</div>
+
+
+<div>
+    <p className="text-secondary font-semibold text-sm">
+      Tecnologias
+    </p>
+    <ul className="list-disc list-inside text-tertiary font-normal indent-5 break-all whitespace-normal">
+      <li>Next.js</li>
+      <li>Tailwind CSS</li>
+      <li>Nextron.js</li>
+      <li>Express.js</li>
+      <li>TypeScript</li>
+      <li>GitHub Actions</li>
+      <li>Figma</li>
+      <li>Sketch</li>
+      <li>Jira</li>
+      <li>Slack</li>
+    </ul>
+</div>
+</div>
+
+
+<div className="flex flex-col container-bg-tertiary w-full h-full py-4 px-4 rounded-md gap-4">
+  <div className="flex flex-row justify-between">
+    <p className="text-primary font-semibold break-all whitespace-normal">
+        Business Intelligence 
+    </p>
+    <SubTitle subTitle="Agosto de 2021 - Abril de 2022" />
+  </div>
+  <div className="flex flex-col justify-between pt-4">
+    <p className="text-secondary font-semibold text-sm">
+      Empresa
+    </p>
+    <SubTitle subTitle="Consorcio Forvias" />
+  </div>
+  <div>
+    <p className="text-secondary font-semibold text-sm">
+      Descrição da vaga
+    </p>
+    <p className="text-tertiary font-normal indent-5 break-all whitespace-normal">
+Realizei a atualização do banco de dados para medições, garantindo que todas as informações estivessem precisas e atualizadas. Revisei e organizei arquivos, assegurando a integridade e acessibilidade dos dados armazenados. Coletei e analisei dados de diversas fontes, utilizando metodologias adequadas para obter insights relevantes. Gerei relatórios detalhados e indicadores de desempenho, facilitando a tomada de decisões informadas.
+</p>
+
+</div>
+
+<div>
+    <p className="text-secondary font-semibold text-sm">
+      Tecnologias
+    </p>
+    <ul className="list-disc list-inside text-tertiary font-normal indent-5 break-all whitespace-normal">
+      <li>Power BI</li>
+      <li>SQL</li>
+      <li>Excel</li>
+      <li>Google Sheets</li>
+      <li>Tableau</li>
+      <li>Jira</li>
+    </ul>
+</div>
+</div>
+
+        </div>
+    </>
+  );
+}
